596963ccbe5b24da4080a329efd123161fd28180
maarquueess <benjaminmarques314@gmail.com>
2025-09-30 16:57:54 -0300

    Update calculadoramedia.java

--- testedemesa/calculadoramedia/calculadoramedia.java
+++ testedemesa/calculadoramedia/calculadoramedia.java
@@ -7,7 +7,6 @@ public class calculadoramedia {
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 
-	        // Leitura das entradas
 	        System.out.print("Digite a nota P1: ");
 	        double P1 = scanner.nextDouble();
 
@@ -26,10 +25,8 @@ public class calculadoramedia {
 	        System.out.print("Digite a nota API: ");
 	        double API = scanner.nextDouble();
 
-	        // Cálculo da Base
 	        double base = (P1 * 0.5) + (E1 * 0.2) + (E2 * 0.3) + X + (SUB * 0.15);
 
-	        // Cálculo da média final
 	        double mediaFinal;
 	        double mediaComAPI = (base * 0.5) + (base > 5.9 ? API * 0.5 : 0);
 
@@ -39,17 +36,15 @@ public class calculadoramedia {
 	            mediaFinal = mediaComAPI;
 	        }
 
-	        // Limitar a nota máxima em 10
 	        if (mediaFinal > 10) {
 	            mediaFinal = 10;
 	        }
 
-	        // Arredondar para 1 casa decimal
 	        mediaFinal = Math.round(mediaFinal * 10.0) / 10.0;
 
-	        // Exibir resultado
 	        System.out.println("A média final do aluno é: " + mediaFinal);
 
 	        scanner.close();
 	    }
 	}
+
