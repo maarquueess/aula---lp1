122b68b72473fef3ff0b1f0b04308e26e6cbeceb
maarquueess <benjaminmarques314@gmail.com>
2025-09-30 16:47:47 -0300

    Update Main.java

--- codigos + fxml/atividade video + fxml/Main.java
+++ codigos + fxml/atividade video + fxml/Main.java
@@ -3,23 +3,15 @@ package atividadevideo;
 public class Main {
     public static void main(String [] args) {
 
-        // Arco: Peso e Tamanho agora são Strings
         Arco arco = new Arco("2.5 kg", "Compósito", "1.2 metros");
 
-        // Flecha: Comprimento agora é String
         Flecha flecha = new Flecha("0.75 m", "Carbono", "Vermelha");
 
-        // Relogio: Permanece o mesmo (usa Strings)
         Relogio relogio = new Relogio("Bateria", "24h", "Preto");
-
-        // --- Teste dos métodos ---
 
         arco.puxar();
         arco.carregar(flecha);
-
-        // Ajustar agora recebe uma String
         arco.ajustar("1.1 m");
-
         arco.soltar(" torre do rei");
 
         relogio.contar();
@@ -31,4 +23,5 @@ public class Main {
         System.out.println(relogio.toString());
 
     }
+
 }
